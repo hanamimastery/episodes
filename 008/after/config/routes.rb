ad03f78298941342d76683046324db0c06a1cfca
Sebastian Wilgosz <sebastian@driggl.com>
2021-07-30 23:28:46 +0200

    Add bulma integration from HME003 to HME008

--- 008/after/config/routes.rb
+++ 008/after/config/routes.rb
@@ -5,8 +5,11 @@ require "hanami/application/routes"
 module HanamiMastery
   class Routes < Hanami::Application::Routes
     define do
-      slice :main, at: "/" do
-        root to: "home.show"
+      slice :main, at: '/' do
+        root to: 'home.show'
+
+        get '/articles', to: 'blog.articles.index'
+        get '/articles/:id', to: 'blog.articles.show'
       end
     end
   end
