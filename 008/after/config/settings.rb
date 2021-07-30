efb97a064c907d5fe5f9b284fc68a4d71c3b41e3
Sebastian Wilgosz <sebastian@driggl.com>
2021-07-30 12:49:02 +0200

    Add base for episode 8

---
+++ 008/after/config/settings.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "hanami_mastery/types"
+require "hanami/application/settings"
+
+module HanamiMastery
+  class Settings < Hanami::Application::Settings
+    # Framework
+    setting :log_to_stdout, constructor: Types::Params::Bool.optional.default(false)
+
+    # Database
+    setting :database_url, constructor: Types::String
+
+    # Application
+    setting :session_secret, constructor: Types::String
+
+    # Assets
+    setting :precompiled_assets, constructor: Types::Params::Bool.optional.default(false)
+    setting :assets_server_url, constructor: Types::String.optional.default("http://localhost:8080")
+  end
+end
---
+++ 008/before/lib/hanami_mastery/repository.rb
@@ -0,0 +1,13 @@
+# auto_register: false
+# frozen_string_literal: true
+
+require "rom-repository"
+require_relative "entities"
+
+module HanamiMastery
+  class Repository < ROM::Repository::Root
+    include Deps[container: "persistence.rom"]
+
+    struct_namespace Entities
+  end
+end
