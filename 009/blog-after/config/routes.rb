f9e56d9351c3a31f7ed41ddd04d2c757b8a81111
Sebastian Wilgosz <sebastian@driggl.com>
2021-09-30 16:04:51 +0200

    Add blog-after for episode HME009

---
+++ 009/blog-after/config/routes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "hanami/application/routes"
+
+module Sandbox
+  class Routes < Hanami::Application::Routes
+    define do
+      slice :main, at: "/" do
+        root to: "blog.articles.index"
+        get '/articles/:id', to: 'blog.articles.show'
+      end
+    end
+  end
+end
---
+++ 009/blog-after/slices/main/lib/main/action.rb
@@ -0,0 +1,9 @@
+# auto_register: false
+# frozen_string_literal: true
+
+require "sandbox/action"
+
+module Main
+  class Action < Sandbox::Action
+  end
+end
