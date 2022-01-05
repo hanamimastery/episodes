ae028092126cfc191d4de74a84f8a35aa70ee896
Sebastian Wilgosz <sebastian@driggl.com>
2022-01-05 11:38:29 +0100

    Add the finalized code for episode HMEP012

--- 012/after/config.ru
+++ 012/after/config.ru
@@ -12,4 +12,7 @@ require "rack/method_override"
 use Rack::MethodOverride
 
 require "hanami/boot"
+
+use JayDoubleuTee::Authorization
+
 run Hanami.app
--- 012/after/slices/main/actions/articles/published.rb
+++ 012/after/slices/main/actions/articles/published.rb
@@ -5,12 +5,21 @@ module Main
     module Articles
       class Published < Main::Action
         include Deps[
-          repo: 'persistence.repositories.articles'
+          repo: 'persistence.repositories.articles',
+          authorizer: 'authorizers.guest'
         ]
+
+        include JayDoubleuTee::Auth
 
         def handle(req, res)
           res.headers['Content-Type'] = 'application/json'
-          res.body = Serializers::Article.new(repo.published).serialize
+
+          if authorizer.call(auth: auth)
+            res.body = Serializers::Article.new(repo.published).serialize
+          else
+            res.body = { error: 'Forbidden request' }
+            res.status = 403
+          end
         end
       end
     end
