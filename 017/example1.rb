2ab45a18f5a540b0b6e905b7b70450e7134827fd
Sebastian Wilgosz <sebastian@driggl.com>
2022-03-07 21:21:59 +0100

    Add code for HME017

---
+++ 017/example1.rb
@@ -0,0 +1,23 @@
+# Does something
+#
+class Foo
+  # Does something cool
+  # @param [Hash] sample argument
+  # @return [NilClass]
+  #
+  def call(arg1)
+  end
+end
+
+# Does something else
+#
+class Bar
+  # Does something cooler
+  # @return [NilClass]
+  #
+  def call
+  end
+end
+
+
+Foo.new.call()
