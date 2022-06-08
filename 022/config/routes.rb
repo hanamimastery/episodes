e3045ff97bc5af8748de07c820e286db7c95fdbf
Sebastian Wilgosz <sebastian@driggl.com>
2022-06-08 02:05:22 +0200

    Add code for HME022 - after

--- 022/config/routes.rb
+++ 022/config/routes.rb
@@ -7,6 +7,20 @@ module Sandbox
     define do
       slice :main, at: "/" do
         root to: "home.show"
+
+        get '/subscriptions',
+          as: :subscriptions,
+          to: ->(env) { [200, {}, [['Awesome Subscriber 1']]] }
+
+        get '/subscriptions/:id',
+          as: :subscription,
+          to: ->(env) { [200, {}, ['Awesome Subscriber 1']] }
+
+        post '/subscribe',
+          as: :subscribe,
+          to: ->(env) { [201, {}, ['Thank You!!!']] }
+
+        redirect '/home', to: '/'
       end
     end
   end
