3e1fb8f2c5925bf5800f182b4777e2ca334639e4
Sebastian Wilgosz <sebastian@driggl.com>
2022-09-14 01:03:26 +0200

    Add HME029 initial code

---
+++ 029/after/app/actions/articles/index.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Sandbox
+  module Actions
+    module Articles
+      class Index < Action
+        include Deps['persistence.rom']
+
+        def handle(req, res)
+          res.status = 200
+          relation = rom.relations[:articles].combine(:author)
+          res.body = relation.to_a.to_json
+        end
+      end
+    end
+  end
+end
