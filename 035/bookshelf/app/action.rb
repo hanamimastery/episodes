b4e4a167ca0348a2ce3fcfb13b57b217823c2e20
Sebastian Wilgosz <sebastian@driggl.com>
2022-12-11 12:41:11 +0100

    Add code for HME035

--- 035/bookshelf/app/action.rb
+++ 035/bookshelf/app/action.rb
@@ -5,5 +5,14 @@ require "hanami/action"
 
 module Bookshelf
   class Action < Hanami::Action
+    handle_exception StandardError => :error_handler
+
+    def error_handler(req, res, exception)
+      res.status = 500
+      res.body = {
+        title: "Server Error",
+        details: exception.message
+      }.to_json
+    end
   end
 end
