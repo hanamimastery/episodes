74e40e09481e32a0e6a7eee9e3785f803bc803aa
Sebastian Wilgosz <sebastian@driggl.com>
2023-11-11 00:27:02 +0100

    Add source for HME026

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -5,10 +5,12 @@ require 'hanami/routes'
 module Sandbox
   class Routes < Hanami::Routes
     define do
-      root { 'Hello from Hanami Mastery!' }
+      root { 'Hello from Hanami' }
 
       get 'messages', to: 'messages.index'
       get 'messages/:id', to: 'messages.show'
+
+      get 'sitemap', to: 'sitemap.show'
     end
   end
 end
