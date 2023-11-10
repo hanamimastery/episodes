f5e5a092586db478df3f47c0f6f8da5efc4b27e2
Sebastian Wilgosz <sebastian@driggl.com>
2023-11-10 17:52:17 +0100

    Add HME003

--- source/slices/main/lib/main/actions/home/show.rb
+++ source/slices/main/lib/main/actions/home/show.rb
@@ -4,6 +4,9 @@ module Main
   module Actions
     module Home
       class Show < Main::Action
+        def handle(_req, res)
+          res.body = '<h1>New way of routing!</h1>'
+        end
       end
     end
   end
