572dc9ae9c7acae8c0b716dc4714471a14cc4c65
Sebastian Wilgosz <sebastian@driggl.com>
2023-11-10 23:08:05 +0100

    Add source code for HME014

---
+++ source/example-from-scratch/nested/boot.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'irb'
+
+require_relative 'lib/my_app/utils/loggers/io_logger'
+require_relative 'lib/my_app/utils/services/subscriptions/email_subscription'
+require_relative 'slices/blog/commands/become_awesome_subscriber'
+
+require 'objspace'
+IRB.start
