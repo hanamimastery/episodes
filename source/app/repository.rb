3efa7ec513d9aa1e9fa6aaa7d4e736583940a944
Sebastian Wilgosz <sebastian@driggl.com>
2023-11-11 00:37:04 +0100

    Add source for HME030

--- source/app/repository.rb
+++ source/app/repository.rb
@@ -6,6 +6,12 @@ module Sandbox
   class Repository < ROM::Repository::Root
     include Deps[container: 'persistence.rom']
 
+    struct_namespace Entities
+
     commands :create
+
+    def all
+      root
+    end
   end
 end
--- source/db/seed.rb
+++ source/db/seed.rb
@@ -3,31 +3,60 @@
 require 'faker'
 
 container = Sandbox::Container
-repo = container['repositories.authors']
+authors = container['repositories.authors']
 
 data = { first_name: 'Seb', last_name: 'Wilgosz', nickname: 'swilgosz' }
-repo.create(data)
+authors.create(data)
 
 data = { first_name: 'Hanami', last_name: 'Master', nickname: 'hm' }
-repo.create(data)
+authors.create(data)
 
 data = { first_name: 'Awesome', last_name: 'Subscriber', nickname: 'awesomesub' }
-repo.create(data)
+authors.create(data)
 
-author_ids = authors.pluck(:id)
+author_ids = authors.all.pluck(:id)
 
 puts author_ids
 
-articles = rom.relations[:articles]
-20.times do
-  articles.changeset(
-    :create,
-    {
-      title: Faker::Hacker.say_something_smart,
-      content: Faker::Lorem.sentences(number: 50).join(' '),
-      status: 0,
-      author_id: author_ids.sample,
-      published_on: Time.now
+articles = container['repositories.articles']
+
+2.times do
+  articles.create(
+    title: Faker::Hacker.say_something_smart,
+    content: Faker::Lorem.sentences(number: 50).join(' '),
+    status: 0,
+    author_id: author_ids.sample,
+    published_on: Time.now
+  )
+end
+
+events = container['repositories.events']
+
+articles.all.each do |article|
+  events.create(
+    type: 'article_published',
+    data: {
+      article_id: article.id,
+      published_at:  Time.now.iso8601,
+      published_by: 1
+    },
+    metadata: {
+      created_at: Time.now.iso8601
     }
-  ).commit
+  )
+end
+
+authors.all.each do |author|
+  events.create(
+    type: 'author_registered',
+    data: {
+      author_id: author.id,
+      first_name: author.first_name,
+      last_name: author.last_name,
+      nickname: author.nickname
+    },
+    metadata: {
+      created_at: Time.now.iso8601
+    }
+  )
 end
