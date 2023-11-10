0d2708b5985dcfc463e8ccc57ee9f12df2e483c2
Sebastian Wilgosz <sebastian@driggl.com>
2023-11-11 00:57:45 +0100

    Add source for HME035

--- source/bookshelf/app/actions/books/show.rb
+++ source/bookshelf/app/actions/books/show.rb
@@ -6,8 +6,16 @@ module Bookshelf
       class Show < Bookshelf::Action
         include Deps['repositories.books']
 
+        params do
+          required(:id).filled(:integer, gteq?: 0)
+        end
+
         def handle(req, res)
+          params = req.params
+          halt 422, params.errors.to_h.to_json unless params.valid?
+
           book = books.find(req.params[:id])
+          halt 404 unless book
           res.body = serialize(book)
         end
 
