57ce8e98cf6a7e5265290c7cca60784a513a148c
Sebastian Wilgosz <sebastian@driggl.com>
2023-11-11 01:08:09 +0100

    Remove code in prep for HME037

--- source/config/slices/api.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module API
-  class Slice < Hanami::Slice
-    import from: :subscribing
-  end
-end
--- source/slices/admin/action.rb
+++
@@ -1,7 +0,0 @@
-# auto_register: false
-# frozen_string_literal: true
-
-module Admin
-  class Action < Hanamimastery::Action
-  end
-end
