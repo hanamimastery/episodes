b396cbb9edcd2bf70bdbe4129db0705808a21522
Sebastian Wilgosz <sebastian@driggl.com>
2023-12-16 20:14:16 +0100

    Add flash messages to the layout

--- source/config/app.rb
+++ source/config/app.rb
@@ -4,6 +4,12 @@ require "hanami"
 
 module Hanamimastery
   class App < Hanami::App
+    config.actions.sessions = :cookie, {
+      key: 'hanamimaster.session',
+      secret: settings.session_secret,
+      expire_after: 60*60*24*365
+    }
+
     config.actions.content_security_policy[:script_src] = [
       'self',
       'https://kit.fontawesome.com',
